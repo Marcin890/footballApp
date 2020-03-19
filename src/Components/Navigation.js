54b7ce31c8e65f6594b4aa1726c165057f23b17d
Marcin Koska <m.koska2@wp.pl>
2020-03-19 22:40:46 +0100

    Change navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 // List of competitions (free API): 2000,2001,2002,2003,2013,2014,2015,2016,2017,2018,2019,2021
@@ -6,56 +6,84 @@ const competitions = [
   {
     id: "2021",
     name: "England",
-    path: "/england"
+    path: "/england",
+    flag: "/england.jpg"
   },
   {
     id: "2003",
     name: "Netherlands",
-    path: "/netherlands"
+    path: "/netherlands",
+    flag: "/netherlands.png"
   },
   {
     id: "2014",
     name: "Spain",
-    path: "/spain"
+    path: "/spain",
+    flag: "/spain.png"
   },
   {
     id: "2002",
     name: "Germany",
-    path: "/germany"
+    path: "/germany",
+    flag: "/germany.png"
   },
   {
     id: "2015",
     name: "France",
-    path: "/france"
+    path: "/france",
+    flag: "/france.png"
   },
   {
     id: "2017",
     name: "Portugal",
-    path: "/portugal"
+    path: "/portugal",
+    flag: "/portugal.png"
   }
 ];
 
-const Navigation = props => {
-  const competitionsList = competitions.map(comp => (
+class Navigation extends Component {
+  state = {
+    isExpanded: false
+  };
+  competitionsList = competitions.map(comp => (
     <li className="nav__list-item" key={comp.id}>
+      <img className="nav__flag" src={comp.flag} alt="" />
+
       <NavLink
         id={comp.id}
         to={{ pathname: comp.id, idComp: comp.id }}
         name={comp.id}
         activeClassName="nav__list-item--active"
+        onClick={() => this.menuToggle()}
       >
         {comp.name}
       </NavLink>
     </li>
   ));
 
-  return (
-    <>
+  menuToggle(e) {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
+  render() {
+    const { isExpanded } = this.state;
+    return (
       <nav className="nav">
-        <ul className="nav__list">{competitionsList}</ul>
+        <button className="nav__toggle" onClick={() => this.menuToggle()}>
+          Menu
+        </button>
+        <ul
+          className={`nav__list--collapsed ${
+            isExpanded ? "nav__list--is-expanded" : ""
+          }`}
+        >
+          {this.competitionsList}
+        </ul>
       </nav>
-    </>
-  );
-};
+    );
+  }
+}
 
 export default Navigation;

5e68edea546e6728c686d0d9922ced92327d3f03
Marcin Koska <m.koska2@wp.pl>
2020-03-28 15:23:48 +0100

    Updates

---
+++ precache-manifest.630d6f2ecb6a6d85a5301e498ad2c0b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5071ca6b471852277d81da8835ce6dd",
+    "url": "/footballApp/index.html"
+  },
+  {
+    "revision": "fc2f5891fdd1bb10e17c",
+    "url": "/footballApp/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "6573cfcf993ab6f0bb30",
+    "url": "/footballApp/static/css/main.7c99febf.chunk.css"
+  },
+  {
+    "revision": "fc2f5891fdd1bb10e17c",
+    "url": "/footballApp/static/js/2.cdfd2b4d.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/footballApp/static/js/2.cdfd2b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6573cfcf993ab6f0bb30",
+    "url": "/footballApp/static/js/main.ee8489d8.chunk.js"
+  },
+  {
+    "revision": "91cdd097fd2fe7d2de1c",
+    "url": "/footballApp/static/js/runtime-main.3d42c492.js"
+  },
+  {
+    "revision": "a5888df1b82426a51380bd173857a26e",
+    "url": "/footballApp/static/media/sort.a5888df1.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/footballApp/precache-manifest.630d6f2ecb6a6d85a5301e498ad2c0b6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/footballApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
